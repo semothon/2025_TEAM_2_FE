d0844c8c73646081cd2a431bde89788321c4ab97
GwonDongHyeon21 <GwonDH21@gmail.com>
2025-03-25 01:18:19 +0900

    Add RoomListScreen

---
+++ app/src/main/java/com/example/team2/model/room.kt
@@ -0,0 +1,6 @@
+package com.example.team2.model
+
+data class Room(
+    val name: String,
+    val details: String
+)
---
+++ app/src/main/java/com/example/team2/roomlist/RoomListScreen.kt
@@ -0,0 +1,63 @@
+package com.example.team2.roomlist
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.team2.model.Room
+
+@Composable
+fun RoomListScreen(viewModel: RoomViewModel = RoomViewModel()) {
+    val rooms by viewModel.rooms.collectAsState(emptyList())
+    val searchQuery by viewModel.searchQuery.collectAsState("")
+
+    Column(modifier = Modifier.fillMaxSize()) {
+        TextField(
+            value = searchQuery,
+            onValueChange = { viewModel.onSearchQueryChanged(searchQuery) },
+            label = { Text(text = "검색") },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        )
+
+        LazyColumn {
+            items(rooms) { room ->
+                RoomItem(room)
+            }
+        }
+    }
+}
+
+@Composable
+fun RoomItem(room: Room) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+    ) {
+        Text(text = room.name)
+        Text(text = room.details)
+        Spacer(modifier = Modifier.height(8.dp))
+        HorizontalDivider()
+    }
+}
+
+@Preview
+@Composable
+fun RoomListPreview() {
+    RoomListScreen()
+}
