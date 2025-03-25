4025f4cc669b31a1baee45d65322531a9fc81d15
GwonDongHyeon21 <GwonDH21@gmail.com>
2025-03-26 00:47:08 +0900

    Update BottomBar Color

--- app/src/main/java/com/example/team2/tabbar/BottomBar.kt
+++ app/src/main/java/com/example/team2/tabbar/BottomBar.kt
@@ -1,13 +1,23 @@
 package com.example.team2.tabbar
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 //noinspection UsingMaterialAndMaterial3Libraries
 import androidx.compose.material.BottomNavigation
 //noinspection UsingMaterialAndMaterial3Libraries
 import androidx.compose.material.BottomNavigationItem
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.example.team2.navigation.BottomNavigationItem
@@ -15,13 +25,15 @@ import com.example.team2.navigation.BottomNavigationItem
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun BottomBar(navController: NavHostController) {
+    var selectedItem by rememberSaveable { mutableStateOf(BottomNavigationItem.Home.title) }
     val bottomItems = listOf(
         BottomNavigationItem.Home,
         BottomNavigationItem.Send,
         BottomNavigationItem.Check,
         BottomNavigationItem.Person
     )
-    BottomNavigation {
+
+    BottomNavigation(backgroundColor = Color.Gray) {
         bottomItems.forEach { item ->
             BottomNavigationItem(
                 icon = {
@@ -30,8 +42,20 @@ fun BottomBar(navController: NavHostController) {
                         contentDescription = item.title
                     )
                 },
-                selected = false,
-                onClick = { navController.navigate(item.title) }
+                selected = selectedItem == item.title,
+                selectedContentColor = Color.Blue,
+                unselectedContentColor = Color.Gray,
+                onClick = {
+                    selectedItem = item.title
+                    navController.navigate(item.title)
+                },
+                modifier = Modifier
+                    .padding(4.dp)
+                    .background(
+                        if (selectedItem == item.title) Color.Blue
+                        else Color.Gray,
+                        RoundedCornerShape(16.dp)
+                    )
             )
         }
     }
