f5d9699bab9d395f793ba1e086bcfb37e343256e
GwonDongHyeon21 <GwonDH21@gmail.com>
2025-03-30 20:00:38 +0900

    Add Keyboard Hide

--- app/src/main/java/com/example/team2/presentation/signup/SignUpVerificationScreen.kt
+++ app/src/main/java/com/example/team2/presentation/signup/SignUpVerificationScreen.kt
@@ -1,41 +1,18 @@
 package com.example.team2.presentation.signup
 
 import android.widget.Toast
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.OutlinedTextFieldDefaults
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import com.example.team2.presentation.component.SignUpCustomText
+import com.example.team2.presentation.component.SignUpRowTextAndButton
 import com.example.team2.presentation.signup.model.SignUp
-import com.example.team2.ui.theme.Brown1
-import com.example.team2.ui.theme.Brown2
-import com.example.team2.ui.theme.Gray3
-import com.example.team2.ui.theme.Gray4
-import com.example.team2.ui.theme.MainColor
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -43,6 +20,7 @@ import kotlinx.coroutines.launch
 @Composable
 fun SignUpVerificationScreen(viewModel: SignUpViewModel) {
     val context = LocalContext.current
+    val keyboardController = LocalSoftwareKeyboardController.current
     val signUpList = viewModel.signUpVerificationList.collectAsState()
     var email by rememberSaveable { mutableStateOf("") }
     var verificationCode by rememberSaveable { mutableStateOf("") }
@@ -51,7 +29,7 @@ fun SignUpVerificationScreen(viewModel: SignUpViewModel) {
 
     Column {
         signUpList.value.forEach { value ->
-            SignUpVerificationTextField(
+            SignUpRowTextAndButton(
                 label = value.label,
                 placeHolder = value.textPlaceholder,
                 buttonText = value.buttonText,
@@ -76,7 +54,8 @@ fun SignUpVerificationScreen(viewModel: SignUpViewModel) {
                         "인증 코드" -> {}
                         "비밀번호" -> {}
                         "비밀번호 확인" -> {
-                            if (password == confirmPassword)
+                            if (password == confirmPassword) {
+                                keyboardController?.hide()
                                 CoroutineScope(Dispatchers.IO).launch {
                                     viewModel.verification(
                                         SignUp(
@@ -87,6 +66,7 @@ fun SignUpVerificationScreen(viewModel: SignUpViewModel) {
                                         )
                                     )
                                 }
+                            }
                             else
                                 Toast.makeText(context, "비밀번호가 일치하지 않습니다.", Toast.LENGTH_SHORT)
                                     .show()
@@ -94,67 +74,6 @@ fun SignUpVerificationScreen(viewModel: SignUpViewModel) {
                     }
                 }
             )
-        }
-    }
-}
-
-@Composable
-fun SignUpVerificationTextField(
-    label: String,
-    placeHolder: String,
-    buttonText: String,
-    value: String,
-    onValueChange: (String) -> Unit,
-    onClick: (String) -> Unit,
-) {
-    Column(modifier = Modifier.fillMaxWidth()) {
-        Spacer(Modifier.height(20.dp))
-        SignUpCustomText(label)
-
-        Spacer(Modifier.height(8.dp))
-        Row(verticalAlignment = Alignment.CenterVertically) {
-            OutlinedTextField(
-                value = value,
-                onValueChange = onValueChange,
-                modifier = Modifier.weight(0.6f),
-                placeholder = {
-                    Text(
-                        text = placeHolder,
-                        style = TextStyle(color = Gray3)
-                    )
-                },
-                visualTransformation =
-                if (label == "비밀번호" || label == "비밀번호 확인")
-                    PasswordVisualTransformation()
-                else
-                    VisualTransformation.None,
-                shape = RoundedCornerShape(8.dp),
-                colors = OutlinedTextFieldDefaults.colors(
-                    focusedBorderColor = Brown1.copy(alpha = 0.2f),
-                    unfocusedBorderColor = Brown1.copy(alpha = 0.2f),
-                ),
-                maxLines = 1
-            )
-
-            Spacer(Modifier.width(8.dp))
-            Button(
-                onClick = { onClick(label) },
-                modifier = Modifier
-                    .weight(0.4f)
-                    .height(55.dp)
-                    .background(
-                        if (value.isNotEmpty()) MainColor else Gray4.copy(alpha = 0.2f),
-                        RoundedCornerShape(8.dp)
-                    ),
-                shape = RoundedCornerShape(8.dp),
-                colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent),
-                enabled = value.isNotEmpty()
-            ) {
-                Text(
-                    text = buttonText,
-                    style = TextStyle(color = Brown2.copy(alpha = if (value.isNotEmpty()) 1f else 0.5f))
-                )
-            }
         }
     }
 }
