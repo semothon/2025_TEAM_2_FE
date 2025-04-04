59a0d55d194a58089ffba5d9ccd1ea5a1779f334
GwonDongHyeon21 <GwonDH21@gmail.com>
2025-04-05 04:05:30 +0900

    Fix confilct

--- app/src/main/java/com/example/team2/presentation/participationlist/model/ParticipationListResponse.kt
+++
@@ -1,8 +0,0 @@
-package com.example.team2.presentation.participationlist.model
-
-import com.google.gson.annotations.SerializedName
-
-data class ParticipationListResponse(
-    @SerializedName("message") val title: String,
-    @SerializedName("groups") val roomDerail: List<ParticipationRoom>
-)
