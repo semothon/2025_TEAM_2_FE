d29d63634c8202cbde9afe4423fbb687a1b96387
GwonDongHyeon21 <GwonDH21@gmail.com>
2025-04-05 04:24:43 +0900

    fix conflict

--- app/src/main/java/com/example/team2/presentation/participationlist/TransactionViewModel.kt
+++ app/src/main/java/com/example/team2/presentation/participationlist/TransactionViewModel.kt
@@ -2,6 +2,6 @@ package com.example.team2.presentation.participationlist
 
 import androidx.lifecycle.ViewModel
 
-class ParticipationListViewModel : ViewModel() {
+class TransactionViewModel : ViewModel() {
 
 }
